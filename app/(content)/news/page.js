d10935f843fb2f17a02df3ee2f4be95c6d802a97
Rohit Bhogal <rbhogal23@gmail.com>
2025-03-14 18:20:30 -0700

    clean up

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -3,20 +3,6 @@ import { getAllNews } from "@/lib/news";
 
 export default async function NewsPage() {
   const news = getAllNews();
-  const Database = require("better-sqlite3");
-  const db = new Database("your-database.db");
-
-  const row = db
-    .prepare(
-      "SELECT name FROM sqlite_master WHERE type='table' AND name='news'"
-    )
-    .get();
-
-  if (row) {
-    console.log("Table exists:", row.name);
-  } else {
-    console.log("Table does NOT exist.");
-  }
 
   return (
     <>
